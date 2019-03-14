00e594316c0ab18832e4344d9f49b19045765fcc
Rishov Chatterjee <garpchoo@gmail.com>
2019-03-14 03:36:30 -0700

    Editing

--- scripts/temperature_viz_reg.py
+++ scripts/temperature_viz_reg.py
@@ -2,4 +2,13 @@ import os
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 from jupyterthemes import jtplot
 
-jtplot.style(theme='onedork') 
+jtplot.style(theme='onedork')
+
+def farenheit(x):
+    return 1.8*x + 32
+
+X = np.arange(-50, 51, 10)
+Y = farenheit(X)
+
+plt.figure(figsize=(12,8))
+plt.scatter(X,Y) 
